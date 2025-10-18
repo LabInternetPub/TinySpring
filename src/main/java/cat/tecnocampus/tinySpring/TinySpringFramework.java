6e2e254c7748793058d32c0a5c607b294e9abcd9
Josep Roure Alcobé <roure@tecnocampus.cat>
2025-10-18 19:55:54 +0200

    moved app code from framework to application

--- src/main/java/cat/tecnocampus/tinySpring/TinySpringFramework.java
+++ src/main/java/cat/tecnocampus/tinySpring/TinySpringFramework.java
@@ -9,7 +9,7 @@ import cat.tecnocampus.tinySpring.webModule.SimpleWebFramework;
 public class TinySpringFramework {
     private static ComponentFactory componentFactory;
 
-    public static void run(Class<?> clazz, String[] args) {
+    public static SimpleWebFramework run(Class<?> clazz, String[] args) {
         //Creates the application context
         componentFactory = new ComponentFactory(clazz.getPackageName() + ".application");
         componentFactory.buildContext();
@@ -23,15 +23,7 @@ public class TinySpringFramework {
         // Creates the web server or framework
         SimpleWebFramework app = new SimpleWebFramework(componentFactory.getContextContainer());
 
-        // With a real web server, these calls would be performed from a REST client
-        System.out.println();
-        HttpRequest request = new HttpRequest("GET", "/hello/how are you");
-        HttpResponse response = app.handleRequest(request);
-        System.out.println(String.format("Response: status-> %s, body-> %s", response.getStatusCode(), response.getBody()));
-
-        System.out.println();
-        request = new HttpRequest("GET", "/hello/TinySpring");
-        response = app.handleRequest(request);
-        System.out.println(String.format("Response: status-> %s, body-> %s", response.getStatusCode(), response.getBody()));
+        return app;
     }
+
 }
